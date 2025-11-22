87caa7a7194d561bb7f4de147fe7534f075e1b78
stellar <fantasyntek@gmail.com>
2025-11-22 16:10:37 +0500

    render

--- js/main.js
+++ js/main.js
@@ -1,46 +1,9 @@
-import { MESSAGES, NAMES } from './data.js';
-import { getRandomNumber, getRandomElement } from './util.js';
+import { createPhoto } from './generator.js';
 
-const createComment = (id) => {
-  return {
-    id: id,
-    avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
-    message: getRandomElement(MESSAGES),
-    name: getRandomElement(NAMES)
-  };
-};
+const photosList = [];
 
-const createComments = () => {
-  const commentsCount = getRandomNumber(0, 30);
-  const comments = [];
+for (let i = 1; i <= 25; i++) {
+  photosList.push(createPhoto(i));
+}
 
-  for (let i = 1; i <= commentsCount; i++) {
-    comments.push(createComment(i));
-  }
-
-  return comments;
-};
-
-const createPhoto = (id) => {
-  return {
-    id: id,
-    url: `photos/${id}.jpg`,
-    description: `Описание фотографии номер ${id}`,
-    likes: getRandomNumber(15, 200),
-    comments: createComments()
-  };
-};
-
-const createPhotos = () => {
-  const photos = [];
-
-  for (let i = 1; i <= 25; i++) {
-    photos.push(createPhoto(i));
-  }
-
-  return photos;
-};
-
-const photosData = createPhotos();
-
-console.log(photosData);
+export { photosList };
